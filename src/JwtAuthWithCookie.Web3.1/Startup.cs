dec76eddd61005794f8931254978ea120be1af97
explorer14 <amanag1983@gmail.com>
2020-06-12 00:02:44 +0200

    Sync repo

---
+++ src/JwtAuthWithCookie.Web3.1/Startup.cs
@@ -0,0 +1,76 @@
+using JwtGenerator.ServiceCollection.Extensions.Cookies;
+using JwtGenerator.Types;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace JwtAuthWithCookie.Web3._1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var key = Guid.NewGuid().ToString();
+            var tokenOptions = new TokenOptions("ISS", "AUD", key);
+
+            services.AddJwtAuthenticationWithProtectedCookie(tokenOptions, "app");
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+
+    public class UserInfo
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public bool HasAdminRights { get; set; }
+    }
+
+    public class UserCredentials
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
