c6879fe805bc4f825496d1ecb4360a2c9b5e364c
explorer14 <amanag1983@gmail.com>
2022-08-14 14:12:02 +0200

    Add a convenience extension to add both the token generator and validator to service collection DI

---
+++ src/JwtHelper.Core/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using JwtHelper.Core.Abstractions;
+using JwtHelper.Core.Types;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JwtHelper.Core.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddJwtHelper(
+            this IServiceCollection services, TokenOptions tokenOptions) =>
+            services.AddTransient<IJwtTokenGenerator>(_ =>
+                    new DefaultJwtTokenGenerator(tokenOptions))
+                .AddTransient<IJwtTokenValidator, DefaultJwtTokenValidator>();
+    }
+}
