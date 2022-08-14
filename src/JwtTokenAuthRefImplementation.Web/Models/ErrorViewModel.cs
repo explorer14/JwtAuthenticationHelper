eab65d5f8caeab1ff85edd218c08d2ecec839cc9
explorer14 <amanag1983@gmail.com>
2022-08-14 12:53:42 +0200

    Move samples into a dedicated folder so as not to make the library projects look confusing

--- src/JwtTokenAuthRefImplementation.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace JwtTokenAuthRefImplementation.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- src/JwtTokenAuthRefImplementation.Web/Models/UserCredentials.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace JwtTokenAuthRefImplementation.Web.Models
-{
-    public class UserCredentials
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
